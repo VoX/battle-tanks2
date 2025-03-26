ebb1b9370b4b8a2ab931abe40a06e59c40c911c2
VoX <v@bitvox.me>
2025-03-26 18:01:13 -0400

    ugh

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,3 +8,59 @@ createRoot(document.getElementById("root")!).render(
     <App />
   </StrictMode>,
 );
+
+
+const client = new WebTransport(
+  `https://localhost:${server.addr.port}/path`,
+  {
+    serverCertificateHashes: [{
+      algorithm: "sha-256",
+      value: new Uint8Array([
+        0x9c, 0x1b, 0x4f, 0x8d, 0x2e, 0x3a, 0x5b, 0x6f, 0x4c, 0x5f,
+        0x4b, 0x1f, 0x2c, 0x3d, 0x4e, 0x5f, 0x6a, 0x7b, 0x8c, 0x9d,
+        0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09,
+      ]),
+    }],
+  },
+);
+
+client.ready.then(async () => {
+  const bi = await client.createBidirectionalStream();
+
+  {
+    const writer = bi.writable.getWriter();
+    await writer.write(new Uint8Array([1, 0, 1, 0]));
+    writer.releaseLock();
+  }
+
+  {
+    const reader = bi.readable.getReader();
+    const response = await reader.read();
+    console.log(response);
+    reader.releaseLock();
+  }
+
+  {
+    const uni = await client.createUnidirectionalStream();
+    const writer = uni.getWriter();
+    await writer.write(new Uint8Array([0, 2, 0, 2]));
+    writer.releaseLock();
+  }
+
+  {
+    const uni =
+      (await client.incomingUnidirectionalStreams.getReader().read()).value;
+    const reader = uni!.getReader();
+    const response = await reader.read();
+    console.log(response);
+    reader.releaseLock();
+  }
+
+  await client.datagrams.writable.getWriter().write(
+    new Uint8Array([3, 0, 3, 0]),
+  );
+  const datagramResponse = await client.datagrams.readable.getReader().read();
+  console.log(datagramResponse);
+
+  client.close();
+});
--- server/main.ts
+++ server/main.ts
@@ -1,6 +1,7 @@
 import { Application } from "jsr:@oak/oak/application";
 import { Router } from "jsr:@oak/oak/router";
 import routeStaticFilesFrom from "./util/routeStaticFilesFrom.ts";
+import { decodeBase64 } from "jsr:@std/encoding@^1.0.5/base64";
 
 export const app = new Application();
 const router = new Router();
@@ -10,6 +11,50 @@ app.use(routeStaticFilesFrom([
   `${Deno.cwd()}/client/dist`,
   `${Deno.cwd()}/client/public`,
 ]));
+
+const cert = Deno.readTextFileSync("localhost.crt");
+const certHash = await crypto.subtle.digest(
+  "SHA-256",
+  decodeBase64(cert.split("\n").slice(1, -2).join("")),
+);
+
+console.log(certHash);
+
+const server = new Deno.QuicEndpoint({
+  hostname: "localhost",
+  port: 9000,
+});
+const listener = server.listen({
+  cert,
+  key: Deno.readTextFileSync("localhost.key"),
+  alpnProtocols: ["h3"],
+});
+
+(async () => {
+  for await (const conn of listener) {
+    const wt = await Deno.upgradeWebTransport(conn);
+
+    wt.ready.then(() => {
+      (async () => {
+        for await (const bidi of wt.incomingBidirectionalStreams) {
+          bidi.readable.pipeTo(bidi.writable).catch(() => {});
+        }
+      })();
+
+      (async () => {
+        for await (const stream of wt.incomingUnidirectionalStreams) {
+          const out = await wt.createUnidirectionalStream();
+          stream.pipeTo(out).catch(() => {});
+        }
+      })();
+
+
+
+      wt.datagrams.readable.pipeTo(wt.datagrams.writable);
+    });
+  }
+})();
+
 
 if (import.meta.main) {
   console.log("Server listening on port http://localhost:8000");
